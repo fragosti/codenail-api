37b503cf33b958c6fbdae0fbc33ebd863f73ba9b
Francesco Agosti <francesco.agosti93@gmail.com>
2017-05-29 16:40:26 -0700

    Add config file helpers and hack

--- config/index.js
+++ config/index.js
@@ -1,11 +1,12 @@
 const getConfig = (env) => {
   switch (env) {
+    default: // on prod it will be undefined
     case 'production':
       return require('./production.js')
-    default:
     case 'development': 
       return require('./development.js')
   }
 }
-
-module.exports = getConfig(process.env.NODE_ENV)
+const config = getConfig(process.env.NODE_ENV)
+console.log(config)
+module.exports = config
